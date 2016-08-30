085e0177fd6546423d353497aae3edd43a2d6085
avarakosov <thelongrunsmoke@gmail.com>
2016-08-30 11:12:42 +0600

    Pass TLE to native, and make SGP4 work.

--- app/src/main/jni/OrbitProcessing.cpp
+++ app/src/main/jni/OrbitProcessing.cpp
@@ -4,13 +4,42 @@
 #include <jni.h>
 
 #include <android/log.h>
+#include <string.h>
 
 #include "OrbitProcessing.h"
+#include "SGP4io.H"
 
 #define APPNAME "ru.tlrs.asciicam"
 
 extern "C"
 jboolean Java_ru_tlrs_iss_MainActivity_tleProcessing(JNIEnv *env, jobject thiz,
                                                      jstring tle) {
+
+    const char *TLE_STRING = env->GetStringUTFChars(tle, 0);
+
+    elsetrec satrec;
+
+    char *tleRows[2];
+    int index = 0;
+    char* token = strtok((char *) TLE_STRING, "\n");
+    while (token != NULL)
+    {
+        tleRows[index++] = token;
+        token = strtok (NULL, "\n");
+    }
+
+    for (int i = 0; i < 2; i++) {
+        __android_log_print(ANDROID_LOG_VERBOSE, APPNAME, "%s", tleRows[i]);
+    }
+
+    double start = 16240;
+    double finish = 16241;
+    double delta = finish - start;
+
+    twoline2rv(tleRows[0], tleRows[1], 'm', 'd', wgs72old, start, finish, delta, satrec);
+
+    __android_log_print(ANDROID_LOG_VERBOSE, APPNAME, "%ld", satrec.satnum);
+
+    env->ReleaseStringUTFChars(tle, TLE_STRING);
     return 0;
 }
